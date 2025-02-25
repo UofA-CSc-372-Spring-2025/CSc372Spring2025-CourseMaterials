7f5aaa047ea8d939bcf711ae9d6fa9f8cc875d99
Michelle Strout <michelle@strout.net>
2025-02-24 18:39:13 -0700

    372 grade perl script appears to be working.

--- Sandboxes/Prolog/372-grade.pl
+++ Sandboxes/Prolog/372-grade.pl
@@ -3,11 +3,14 @@
 Example query:
 
 ?-  Goal=800, SAs=[15,0,15], LAs=[0],  ICAs=[20], MT1=50, Extra=0.
-?- sum($SAs,SAtotal), sum($LAs,LAtotal), sum($ICAs,ICAtotal),
-can_reach_goal(SAtotal, LAtotal, ICAtotal, $MT1, $Extra, $Goal,
-                InClassCurrPoints, TotalCurrPoints,
-                InClassNeededToPass, InClassPossible,
-                OutOfClassNeededToPass, OutOfClassPossible).
+?- sum($SAs,SAtotal), sum($LAs,LAtotal), sum($ICAs,ICAtotal).
+?- can_reach_goal($SAtotal, $LAtotal, $ICAtotal, $MT1, $Extra, $Goal,
+                InClassCurrPoints, OutClassCurrPoints, TotalCurrPoints,
+                MoreInClassNeededForGoal, InClassPossible,
+                MoreOutOfClassNeededForGoal, OutOfClassPossible).
+
+SAs=[100], LAs=[100],  ICAs=[100], MT1=70, Extra=12.
+FIXME: InClassNeededToPass = 290, This is a big wonky in this result.  What is happening with the ICAs?
 */
 
 % Calculates total points from a list
@@ -16,9 +19,9 @@ sum([H|T], Total) :- sum(T, Rest), Total is H + Rest.
 
 % Determines whether a student has enough points to pass and/or reach a goal
 can_reach_goal(SA, LA, ICA, MT1, Extra, Goal,
-               InClassCurrPoints, TotalCurrPoints,
-               InClassNeededForGoal, InClassPossible,
-               OutOfClassNeededForGoal, OutOfClassPossible) :-
+               InClassCurrPoints, OutClassCurrPoints, TotalCurrPoints,
+               MoreInClassNeededForGoal, InClassPossible,
+               MoreOutOfClassNeededForGoal, OutOfClassPossible) :-
 
     % Define point requirements
     InClassPassRequirement = 360,
@@ -29,41 +32,42 @@ can_reach_goal(SA, LA, ICA, MT1, Extra, Goal,
     SAleft = 75, LAleft = 80, FPleft = 200, ICAleft = 50, 
     MT2left = 200, FinalExamLeft = 200, ExtraMax = 40,
 
-    % Max category limits
-    SAmax = 100, LAmax = 100, ICAmax = 100,
-
     % Calculate current points
     FP = 0, MT2 = 0, FinalExam = 0,
     total_points(SA, LA, ICA, MT1, Extra, MT2, FP, FinalExam,
                  InClassCurrPoints, OutClassCurrPoints, TotalCurrPoints),
 
     % Calculate needed points for passing
-    InClassNeededToPass is max(0, InClassPassRequirement - InClassCurrPoints),
-    OutOfClassNeededToPass is max(0, TotalPassRequirement
-                                     - InClassPassRequirement
-                                     - OutClassCurrPoints),
+    MoreInClassNeededToPass is max(0, InClassPassRequirement - InClassCurrPoints),
+    MoreOutOfClassNeededToPass is max(0, TotalPassRequirement
+                                         - InClassPassRequirement
+                                         - OutClassCurrPoints),
+
+    % Max category limits
+    SAmax = 100, LAmax = 100, ICAmax = 100,
 
     % Calculate possible points left
-    ICA_Possible is min(ICAleft, ICAmax - ICA),
-    SA_Possible is min(SAleft, SAmax - SA),
-    LA_Possible is min(LAleft, LAmax - LA),
+    ICA_Possible is min(0,min(ICAleft, ICAmax - ICA)),
+    SA_Possible is min(0,min(SAleft, SAmax - SA)),
+    LA_Possible is min(0,min(LAleft, LAmax - LA)),
     InClassPossible is ICA_Possible + MT2left + FinalExamLeft,
     OutOfClassPossible is SA_Possible + LA_Possible + FPleft
                           + (ExtraMax - Extra),
 
-    % Additional points needed for the target goal.
-    %DeltaOver600Needed is max(0, Goal - 600),
     
-    % Distribute needed extra points across in-class and out-of-class.
+    % Distribute needed more points across in-class and out-of-class.
     % Going to use up OutOfClassPossible first, then InClassPossible.
-    OutOfClassNeededForGoal is max(OutOfClassNeededToPass,
-                                   min(OutOfClassPossible, Goal-InClassNeededToPass)),
-    InClassNeededForGoal is max(InClassNeededToPass,
-                                Goal - OutOfClassNeededForGoal - TotalCurrPoints),
+    MoreOutOfClassNeededForGoal is
+        max(MoreOutOfClassNeededToPass,
+            min(OutOfClassPossible,
+                Goal-MoreInClassNeededToPass-TotalCurrPoints)),
+    MoreInClassNeededForGoal is
+        max(MoreInClassNeededToPass,
+            Goal - MoreOutOfClassNeededForGoal - TotalCurrPoints),
 
     % Ensure the goal is achievable
-    InClassNeededForGoal =< InClassPossible,
-    OutOfClassNeededForGoal =< OutOfClassPossible.
+    MoreInClassNeededForGoal =< InClassPossible,
+    MoreOutOfClassNeededForGoal =< OutOfClassPossible.
 
 % define totalPoints predicate that caps SAtotal, LAtotal, and ICAtotal to 100,
 % Extra to 40, and calculates InClassPoints, OutClassPoints, and TotalPoints.
@@ -73,7 +77,6 @@ total_points(SAtotal, LAtotal, ICAtotal, MT1, Extra, MT2, FP, FinalExam,
     SA_Capped is min(SAtotal, 100),
     LA_Capped is min(LAtotal, 100),
     Extra_Capped is min(Extra, 40),
-    OutClassPoints is SA_Capped + LA_Capped + FP,
-    InClassPoints is ICA_Capped + MT1 + MT2 + FinalExam + Extra_Capped,
+    InClassPoints is ICA_Capped + MT1 + MT2 + FinalExam,
+    OutClassPoints is SA_Capped + LA_Capped + FP + Extra_Capped,
     TotalPoints is InClassPoints + OutClassPoints.
-
